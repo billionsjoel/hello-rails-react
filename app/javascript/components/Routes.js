6e5c2325d01273abe050aa734440a728fde8653e
Atugonza ( Billions ) Joel <joelatugonza@gmail.com>
2022-06-16 15:01:59 +0300

    create routes js

---
+++ app/javascript/components/Routes.js
@@ -0,0 +1,20 @@
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { Route, Routes } from 'react-router-dom';
+import Greeting from './Greeting';
+import { useDispatch } from 'react-redux';
+import { fetchRandomMessage } from './redux/message';
+const MyRoutes = () => {
+	const dispatch = useDispatch();
+	useEffect(() => {
+		dispatch(fetchRandomMessage());
+	}, []);
+
+	return (
+		<Routes>
+			<Route path="/" element={<Greeting />} />
+		</Routes>
+	);
+};
+
+export default MyRoutes;
